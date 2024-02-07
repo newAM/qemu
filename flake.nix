c2ecaa886a731043d0b30487770b9ff198be060c
Alex Martens <alex@thinglab.org>
2024-02-06 17:09:40 -0800

    add development shell

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      inputsFrom = [pkgs.qemu_full];
+    };
+  };
+}
